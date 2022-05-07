459db704771784563f03893685f40fd6f0651c9a
ArindamDutta02082001 <83761396+ArindamDutta02082001@users.noreply.github.com>
2022-05-07 10:37:11 +0530

    ok

--- array/returnmaxSubArraySum.cpp
+++ array/returnmaxSubArraySum.cpp
@@ -1,40 +1,20 @@
-#include <bits/stdc++.h>
-using namespace std;
-// ***********Correct but gfg generated some random errors for huge input file******************
-long long maxSubarraySum(int[], int);
-
-int main()
+// link:https://leetcode.com/problems/maximum-subarray/
+int maxSubArray(vector<int> &nums)
 {
 
-    int arr[] = {-1, -2, -3, -4};
-    int l = 4;
-    cout<<maxSubarraySum(arr, l);
-    return 0;
-}
-
-long long maxSubarraySum(int arr[], int n)
-{
-
-    int temp[n];
     int s = 0;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < nums.size(); i++)
     {
-        s += arr[i];
-        if (s >= 0)
-            temp[i] = s;
-        if (s < 0)
+        s += nums[i];
+        if (s <= 0)
         {
-            temp[i] = arr[i];
+            nums[i] = s;
             s = 0;
         }
+        else
+        {
+            nums[i] = s;
+        }
     }
-
-    long long max = -9999999999;
-    for (int i = 0; i < n; i++)
-    {
-        if (temp[i] > max)
-            max = temp[i];
-    }
-
-    return max;
+    return *max_element(nums.begin(), nums.end());
 }
